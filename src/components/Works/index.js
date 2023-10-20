7618bc1a02c98af807a90bc4e363af7b15c5791c
Hanna Rosenfeld <hannarosenfeld@Hannas-Air.lan>
2023-10-20 09:25:50 -0400

    Paintings only 14-28.

--- src/components/Works/index.js
+++ src/components/Works/index.js
@@ -4,19 +4,6 @@ import { Carousel } from 'react-responsive-carousel';
 import "./Works.css"
 
 // Paintings
-import P1 from './Paintings/_MG_2606.jpg';
-import P2 from './Paintings/_MG_2607.jpg';
-import P3 from './Paintings/_MG_2609.jpg'
-import P4 from './Paintings/_MG_2612.jpg'
-import P5 from './Paintings/_MG_2615.jpg'
-import P6 from './Paintings/_MG_2616.jpg'
-import P7 from './Paintings/_MG_2618.jpg'
-import P8 from './Paintings/_MG_2621.jpg'
-import P9 from './Paintings/_MG_2623.jpg'
-import P10 from './Paintings/_MG_2625.jpg'
-import P11 from './Paintings/_MG_2628.jpg'
-import P12 from './Paintings/_MG_2629.jpg'
-import P13 from './Paintings/_MG_2632.jpg'
 import P14 from './Paintings/_MG_2634.jpg'
 import P16 from './Paintings/_MG_2635.jpg'
 import P17 from './Paintings/_MG_2636.jpg'
@@ -31,7 +18,6 @@ import P25 from './Paintings/Ewald_Janz_08.jpg'
 import P26 from './Paintings/Ewald_Janz_09.jpg'
 import P27 from './Paintings/Ewald_Janz_13.jpg'
 import P28 from './Paintings/Ewald_Janz_16.jpg'
-import P29 from './Paintings/sdnsa.jpg'
 
 // Drawings
 import D1 from './Drawings/img012.jpg'
@@ -62,19 +48,6 @@ export default function Works({ category }) {
   const [activeTab, setActiveTab] = useState(category ? category : 'paintings');
 
   const paintingsImages = [
-    // { name: 'Painting 1', url: P1 },
-    // { name: 'Painting 2', url: P2 },
-    // { name: 'Painting 3', url: P3 },
-    // { name: 'Painting 4', url: P4 },
-    // { name: 'Painting 5', url: P5 },
-    // { name: 'Painting 6', url: P6 },
-    // { name: 'Painting 7', url: P7 },
-    // { name: 'Painting 8', url: P8 },
-    // { name: 'Painting 9', url: P9 },
-    // { name: 'Painting 10', url: P10 },
-    // { name: 'Painting 11', url: P11 },
-    // { name: 'Painting 12', url: P12 },
-    // { name: 'Painting 13', url: P13 },
     { name: 'Painting 14', url: P14 },
     { name: 'Painting 16', url: P16 },
     { name: 'Painting 17', url: P17 },
@@ -89,7 +62,6 @@ export default function Works({ category }) {
     { name: 'Painting 26', url: P26 },
     { name: 'Painting 27', url: P27 },
     { name: 'Painting 28', url: P28 },
-    // { name: 'Painting 29', url: P29 },
   ];
 
   const drawingsImages = [
